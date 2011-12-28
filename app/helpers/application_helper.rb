96297c6ff94a5a1960cb71cb2720435adeef173a
Tasos Laskos <tasos.laskos@gmail.com>
2011-12-29 01:50:07 +0200

    added modules(): returns all keystone modules

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,32 @@
 module ApplicationHelper
 
-  def title
-    if controller.action_name == "index"
-      "All #{controller.controller_name.titleize}"
-    else
-      "#{controller.action_name.titleize} #{controller.controller_name.titleize.singularize}"
+    def modules
+        return @@modules if !(@@modules ||= {}).empty?
+
+        Rails::Application::Railties.engines.each {
+            |engine|
+            mod = const_get( engine.class.to_s.split( "::" ).first )
+            next if !mod.respond_to?( :keystone? )
+            @@modules[mod] = engine
+        }
+        @@modules
     end
-  end
-  
-  def create_or_update_string
-    if controller.action_name == "new"
-      "Create"
-    else
-      "Update"
+
+    def title
+        if controller.action_name == "index"
+            "All #{controller.controller_name.titleize}"
+        else
+            "#{controller.action_name.titleize} #{controller.controller_name.titleize.singularize}"
+        end
     end
-  end
-  
+
+    def create_or_update_string
+        if controller.action_name == "new"
+            "Create"
+        else
+            "Update"
+        end
+    end
+
+    extend self
 end
