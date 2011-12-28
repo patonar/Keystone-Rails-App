499a49437e7d95c347b8754cac78fb21467e1289
Tasos Laskos <tasos.laskos@gmail.com>
2011-12-29 01:55:18 +0200

    removed arachni()

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,10 @@
 class PagesController < ApplicationController
-  before_filter :authenticate_user!
+    before_filter :authenticate_user!
 
-  def index
-  end
+    def index
+    end
 
-  def help
-  end
-  
-  def arachni
-  end
-  
+    def help
+    end
+
 end
